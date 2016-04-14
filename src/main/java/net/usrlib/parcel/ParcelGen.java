8be9e24997dd88ec551e320fa7d60c4d50b277de
rgr-myrg <rpmyrg@gmail.com>
2016-04-14 12:22:06 -0400

    Added license.

--- src/main/java/net/usrlib/parcel/ParcelGen.java
+++ src/main/java/net/usrlib/parcel/ParcelGen.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2016. UsrLib.Net
+ *
+ * Licensed under the Apache License,  Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *@author https://github.com/rgr-myrg
+ */
 package net.usrlib.parcel;
 
 import java.io.IOException;
@@ -8,6 +25,7 @@ import org.json.JSONObject;
 public class ParcelGen {
 	public static final String CMD_INPUT_FILE = "-i";
 	public static final String CMD_OUTPUT_FILE = "-o";
+	public static final String TEMPLATE_FILENAME = "src/main/resources/template.txt";
 
 	public static void main (String[] args) throws InvalidInputException {
 		final CommandLineOptions options = new CommandLineOptions(new String[] {CMD_INPUT_FILE, CMD_OUTPUT_FILE});
@@ -44,7 +62,7 @@ public class ParcelGen {
 			throw new InvalidInputException("Unable to parse json file. Ensure input file contains valid json");
 		}
 
-		final Template template = new Template("src/main/resources/template.txt");
+		final Template template = new Template(TEMPLATE_FILENAME);
 		boolean hasTemplate = false;
 
 		try {
